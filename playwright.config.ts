7269460775b4c14de1b26a57b396cb9867430774
Itai Agmon <itai.agmon@gmail.com>
2026-06-30 09:11:54 +0300

    Permissions and timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  
+  timeout: 5000,
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
